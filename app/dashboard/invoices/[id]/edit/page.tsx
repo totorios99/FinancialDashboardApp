4608050b910d6e723efab599889cdbf85035d14d
Antonio Rios <joseantonio_ra@live.com>
2024-03-20 11:54:03 -0600

    fix(update): edit form route corrected

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -18,7 +18,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Invoices', href: '/invoices' },
+          { label: 'Invoices', href: '/dashboard/invoices' },
           {
             label: 'Edit invoice',
             href: `/dashboard/invoices/${id}/edit`,
